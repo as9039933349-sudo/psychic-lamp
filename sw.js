e88ca217a05e3431e5d49baacbea951df91e2246
as9039933349-sudo <as9039933349@gmail.com>
2026-08-24 17:50:22 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='bb-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
